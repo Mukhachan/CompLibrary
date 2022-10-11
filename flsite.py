37992f61768bc17b1c7b8b237be509b96c416e43
Mukhamet1996 <aralmuhamet@gmail.com>
2022-10-11 22:05:25 +0300

    Комментарии и изменение кнопки назад (спать хочу)

--- flsite.py
+++ flsite.py
@@ -13,12 +13,11 @@ app.config.from_object(__name__)
 app.config.update(dict(DATABASE=os.path.join(app.root_path, 'flsite.db')))
 
 
-
+#  Создание, соединение и получение данных БД  #
 def connect_db():
     conn = sqlite3.connect(app.config['DATABASE'])
     conn.row_factory = sqlite3.Row
     return conn
-
 def create_db():
     '''Вспомогательная функция для создания таблиц бд'''
     db = connect_db()
@@ -26,7 +25,6 @@ def create_db():
         db.cursor().executescript(f.read())
     db.commit()
     db.close()
-
 def get_db():
     '''Соединение с бд, если оно ещё не установлено '''
     if not hasattr(g, 'link_dv'):
@@ -34,7 +32,7 @@ def get_db():
     return g.link_db
 
 
-
+#  редирект на страницу рекомендации  #
 @app.route('/')
 def index():
     db = get_db()
@@ -42,25 +40,29 @@ def index():
     print( url_for('index') )
     return redirect(url_for('recommended'))
 
+#  Страница рекомендации (по сути главная)  #
 @app.route('/recommended')
 def recommended():
     db = get_db()
     dbase = FDataBase(db)
     return render_template('index.html', css_link='styles.css', menu=dbase.getMenu())
 
-
+#  Закрытие соединения с базой данных  #
 @app.teardown_appcontext
 def closed_db(error):
     """Закрытие соединения"""
     if hasattr(g, 'link_db'):
         g.link_db.close()
 
+#  Страница "о библиотеке"  #
 @app.route('/about')
 def about():
     db = get_db()
     dbase = FDataBase(db)
     return render_template('about.html', menu=dbase.getMenu())
 
+
+#  руты авторизации и регистрации  #
 @app.route('/auth', methods=["POST", "GET"])
 def auth():
     if request.method == 'POST':
@@ -73,6 +75,8 @@ def register():
         print(request.form)
     return render_template('register.html')    
 
+
+#  Обработка ошибок  #
 @app.errorhandler(404)
 def PageNotFound(error):
     db = get_db()
@@ -80,6 +84,7 @@ def PageNotFound(error):
     return render_template('page404.html', title='Страница не найдена', 
                             menu=dbase.getMenu(), css_link = 'styles.css')
 
+#  Добавление книги  #
 @app.route('/newbook', methods=["POST", "GET"])
 def newbook():
     if request.method == 'POST':
